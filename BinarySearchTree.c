d77f3fb36fb352de6deb8831b0ccfa694e2721e5
Dimpal Shetty <46956063+dimpalshetty@users.noreply.github.com>
2021-09-03 09:43:34 +0530

    Create BinarySearchTree.c

---
+++ BinarySearchTree.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node {
+    int data;
+    struct node *left, *right;
+    
+};
+
+
+struct node *newnode(int item){
+    struct node *temp=(struct node *)malloc(sizeof(struct node));
+    temp->data=item;
+    temp->left=temp->right=NULL;
+    return temp;
+}
+
+void inorder(struct node *root){
+    if(root!=NULL){
+        inorder(root->left);
+        printf("%d->", root->data);
+        inorder(root->right);
+    }
+}
+
+struct node *insert(struct node *node, int data){
+    if(node==NULL){
+       return newnode(data);
+    }
+    if(data>node->data){
+        node->right=insert(node->right,data);
+    }
+    else {
+        node->left=insert(node->left,data);
+    }
+    return node;
+}
+
+
+int main(){
+    
+    struct node *root=NULL;
+    root=insert(root,8);
+  root = insert(root, 3);
+  root = insert(root, 1);
+  root = insert(root, 6);
+  root = insert(root
