a15cd32590cfad9f2c13600c879f4fb616390112
Dimpal Shetty <46956063+dimpalshetty@users.noreply.github.com>
2021-09-03 09:44:07 +0530

    Update BinarySearchTree.c

--- BinarySearchTree.c
+++ BinarySearchTree.c
@@ -39,9 +39,13 @@ struct node *insert(struct node *node, int data){
 
 int main(){
     
-    struct node *root=NULL;
+  struct node *root=NULL;
     root=insert(root,8);
   root = insert(root, 3);
   root = insert(root, 1);
   root = insert(root, 6);
-  root = insert(root
+  root = insert(root, 7);
+  root = insert(root, 10);
+  root = insert(root, 14);
+  root = insert(root, 4);
+}
